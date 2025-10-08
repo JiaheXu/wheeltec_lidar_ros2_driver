8b7e622b08ca64aa3809d69d00cdce9ccb1d109d
Jiahe Xu <xjh4438318846@gmail.com>
2025-10-08 03:12:08 -0400

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 sudo apt-get install ros-humble-diagnostic-updater libpcap-dev -y
 
-sudo sh ./src/wheeltec_udev.sh
+#Don't use, not working yet
+#sudo cp  ./src/wheeltec_controller.rules /etc/udev/rules.d/
+#sudo cp  ./src/wheeltec_lidar.rules /etc/udev/rules.d/
+
+sudo service udev reload
+sleep 2
+sudo service udev restart
+
+developer@ubuntu:~$ ros2 topic info /scan
+Type: sensor_msgs/msg/LaserScan
+Publisher count: 1
+Subscription count: 0
+
